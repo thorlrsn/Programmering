5a6b53749299b5e37c16faf08d9b89d9be09fbe9
thorlrsn <55739873+thorlrsn@users.noreply.github.com>
2019-11-21 12:11:35 +0100

    Add files via upload

---
+++ Oak.java
@@ -0,0 +1,16 @@
+package project3;
+
+import java.awt.Point;
+
+public class Oak extends Tree {
+
+	private Point pos;
+	private double size;
+
+	public Oak(Point pos, double size) {
+		this.pos = pos;
+		this.size = size;
+	}
+	
+	
+}
---
+++ Tree.java
@@ -0,0 +1,28 @@
+package project3;
+
+import java.awt.Color;
+
+public class Tree {
+	
+	public void grow() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Color getColor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getPosition() {
+		// TODO Auto-generated method stub
+		x = pos.x;
+		return null;
+	}
+
+	public double getSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
