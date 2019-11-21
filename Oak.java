206d7bb9af00bfbcd61dc48690220d491fc72142
thorlrsn <55739873+thorlrsn@users.noreply.github.com>
2019-11-21 12:12:45 +0100

    Update Oak.java

--- Oak.java
+++ Oak.java
@@ -1,16 +1,31 @@
 package project3;
 
+import java.awt.Color;
 import java.awt.Point;
 
 public class Oak extends Tree {
 
-	private Point pos;
-	private double size;
+	protected Point pos;
+	protected double size;
 
 	public Oak(Point pos, double size) {
 		this.pos = pos;
 		this.size = size;
 	}
 	
+	public Color OakColor() {
+		return Color.blue;
+	}
+
+	public double growth() {		
+		size = size*6;
+		return size;
+	}
 	
+	public double getSize() {
+		return size;
+	}
+
+
+
 }
